42853839dfef4faba5753f56291f934cddc5578e
Delia <delia.bute@yahoo.com>
2021-10-19 12:42:07 +0100

    feat: added form that user can add a space

--- spec/features/add_space_spec.rb
+++ spec/features/add_space_spec.rb
@@ -8,4 +8,15 @@ feature "Adding space" do
     visit "/spaces/new"
     expect(page).to have_content "List a Space"
   end
+
+  scenario "Page displays a form that user can input onto" do
+    visit "/spaces/new"
+    fill_in "name", with: "Central apartment"
+    fill_in "description", with: "Great apartment in city centre, 2 minute walk from the tube station"
+    fill_in "price_night", with: "50"
+    click_button "Add Space"
+    expect(page).to have_content "Central apartment"
+    expect(page).to have_content "Great apartment in city centre, 2 minute walk from the tube station"
+    expect(page).to have_content "50"
+  end
 end
