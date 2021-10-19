53693e44ae4cfc5e002a61d42fa1cc884d18a058
frontendowl <frontendowl@gmail.com>
2021-10-19 15:49:21 +0100

    FIX: change db name

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,6 +1,6 @@
 require 'pg'
 
 def setup_test_database
-  connection = PG.connect(dbname: 'spaces_test')
+  connection = PG.connect(dbname: 'makers_bnb_test')
   connection.exec('TRUNCATE spaces;')
 end
