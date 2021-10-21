80fc10f34b6e5685473582cd1414631970d3f519
Xia <zeyaoliu@me.com>
2021-10-21 03:25:17 +0100

    style: add link to Requests page in nav bar add styling to Requests page add divs for made & received requests change /space/ route to /space to be consistent with mockups change /request route to /requests to be consistent with mockup apply change to all relevant files to ensure all tests pass

--- spec/features/list_spaces_spec.rb
+++ spec/features/list_spaces_spec.rb
@@ -1,14 +1,14 @@
-require 'pg'
+require "pg"
 
 feature "It displays spaces" do
   scenario "a guest can view all spaces in a single page" do
-    add_space(name: 'Alex House', description: 'Spacious and cosy', price: 20)
-    add_space(name: 'Ewa Mansion', description: 'Modern and well-lit', price: 100)
+    add_space(name: "Alex House", description: "Spacious and cosy", price: 20)
+    add_space(name: "Ewa Mansion", description: "Modern and well-lit", price: 100)
     # connection = PG.connect(dbname: 'makers_bnb_test')
     # connection.exec("INSERT INTO spaces (name, description, price) VALUES ('Alex House', 'Spacious and cosy', 20);")
     # connection.exec("INSERT INTO spaces (name, description, price) VALUES ('Ewa Mansion', 'Modern and well-lit', 100);")
 
-    visit "/spaces/"
+    visit "/spaces"
     expect(page).to have_content "Alex House"
     expect(page).to have_content "Spacious and cosy"
     expect(page).to have_content "20"
@@ -17,4 +17,3 @@ feature "It displays spaces" do
     expect(page).to have_content "100"
   end
 end
-    
